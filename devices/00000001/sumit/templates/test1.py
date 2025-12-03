a33b38b1b303ea47417aa6af0667b9094126cd61
Shinnosuke Yabe <shinnosuke.yabe@aeterlink.com>
2025-12-03 17:39:36 +0900

    commit 11, 12

--- devices/00000001/sumit/templates/test1.py
+++ devices/00000001/sumit/templates/test1.py
@@ -1,7 +1,1 @@
-# sumit 00000001 test1.py
-# second coment for 00000001
-# third coment for 00000001
-# forth coment for 00000001
-# fifth coment for 00000001
-# sixth coment for 00000001
-# seventh coment for 00000001
+# commit 202512031738
